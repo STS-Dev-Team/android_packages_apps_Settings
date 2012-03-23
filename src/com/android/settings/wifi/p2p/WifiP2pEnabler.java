STS-Dev-Team/android_packages_apps_Settings

commit 4541d9053bb85190dbd845aebba988eeb64b0ba8

Fixed wrong "WiFi Direct" state after Tablet wakes up
"WiFi Direct" feature unexpectedly goes to disabled state after
wake up though it was enabled before suspend. Fixed now.

Change-Id: Iced7f48f293aeb49411a998b7043882961e07873
Signed-off-by: Anatolii.Shuba <x0158321@ti.com>

src/com/android/settings/wifi/p2p/WifiP2pEnabler.java
@@ -28,6 +28,8 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.provider.Settings;
 import android.util.Log;
+import android.net.wifi.WifiManager;
+import android.os.SystemProperties;
 
 /**
  * WifiP2pEnabler is a helper to manage the Wifi p2p on/off
@@ -40,6 +42,7 @@ public class WifiP2pEnabler implements Preference.OnPreferenceChangeListener {
     private final IntentFilter mIntentFilter;
     private WifiP2pManager mWifiP2pManager;
     private WifiP2pManager.Channel mChannel;
+    private WifiManager mWifiManager;
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -57,6 +60,9 @@ public class WifiP2pEnabler implements Preference.OnPreferenceChangeListener {
         mContext = context;
         mCheckBox = checkBox;
 
+        if (SystemProperties.OMAP_ENHANCEMENT) {
+            mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        }
         mWifiP2pManager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
         if (mWifiP2pManager != null) {
             mChannel = mWifiP2pManager.initialize(mContext, mContext.getMainLooper(), null);
@@ -92,6 +98,9 @@ public class WifiP2pEnabler implements Preference.OnPreferenceChangeListener {
         mCheckBox.setEnabled(false);
         final boolean enable = (Boolean) value;
         if (enable) {
+            if (SystemProperties.OMAP_ENHANCEMENT && mWifiManager != null) {
+                mWifiManager.setWifiEnabled(false);
+            }
             mWifiP2pManager.enableP2p(mChannel);
         } else {
             mWifiP2pManager.disableP2p(mChannel);

commit dc5a97983fc17fd73a8e4be6249a44162b0df160

WiFi: Side effect fixed from the "Fixed wrong "WiFi Direct" state..."
This patch fixes side effect which was introduced by
4541d9053bb85190dbd845aebba988eeb64b0ba8 commit ID.

Side effect description:
If Wi-Fi is ON and we try to enable Wi-Fi Direct,
WiFi should be turned Off only if WiFi Direct selection confirmed
by user. But WiFi is always turned Off despite the user selection.
Side effect has took place because of wrong placement of the fix
mentioned above.

Change-Id: I4b996c3c8fc09498100657cb5dd8380a5944f6df
Signed-off-by: Anatolii.Shuba <x0158321@ti.com>

src/com/android/settings/wifi/p2p/WifiP2pEnabler.java
@@ -98,9 +98,6 @@ public class WifiP2pEnabler implements Preference.OnPreferenceChangeListener {
         mCheckBox.setEnabled(false);
         final boolean enable = (Boolean) value;
         if (enable) {
-            if (SystemProperties.OMAP_ENHANCEMENT && mWifiManager != null) {
-                mWifiManager.setWifiEnabled(false);
-            }
             mWifiP2pManager.enableP2p(mChannel);
         } else {
             mWifiP2pManager.disableP2p(mChannel);
@@ -113,6 +110,9 @@ public class WifiP2pEnabler implements Preference.OnPreferenceChangeListener {
         switch (state) {
             case WifiP2pManager.WIFI_P2P_STATE_ENABLED:
                 mCheckBox.setChecked(true);
+                if (SystemProperties.OMAP_ENHANCEMENT && mWifiManager != null) {
+                    mWifiManager.setWifiEnabled(false);
+                }
                 break;
             case WifiP2pManager.WIFI_P2P_STATE_DISABLED:
                 mCheckBox.setChecked(false);